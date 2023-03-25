8beb6946d252623fcff118c13a0a6653a649a635
J Mohamed RIyas <116546879+mohamedriyasjahaburdeen@users.noreply.github.com>
2023-03-25 13:59:55 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,37 @@
+var jsonData = [
+    {
+        "q" : "1) How long is an IPv6 address?",
+        "opt1" : "32 bits",
+        "opt2" : "128 bytes",
+        "opt3" : "128 bits",
+        "answer" : "128 bits"
+    },
+    {
+        "q" : "2) HTML stands for?",
+        "opt1" : "Hyper Text Markup Language",
+        "opt2" : "High Text Markup Language",
+        "opt3" : "Hyper Tabular Markup Language",
+        "answer" : "Hyper Text Markup Language"
+    },
+    {
+        "q" : "3) which of the following tag is used to mark a begining of paragraph ?",
+        "opt1" : "TD",
+        "opt2" : "br",
+        "opt3" : "P",
+        "answer" : "P"
+    },
+    {
+        "q" : "4) From which tag descriptive list starts ?",
+        "opt1" : "LL",
+        "opt2" : "DL",
+        "opt3" : "DD",
+        "answer" : "DL"
+    },
+    {
+        "q" : "5) Correct HTML tag for the largest heading is _____",
+        "opt1" : "h1",
+        "opt2" : "h6",
+        "opt3" : "heading",
+        "answer" : "h1"
+    }
+];
